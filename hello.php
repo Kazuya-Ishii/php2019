1c516c293f3a37e30802a7f6a0ce397fccada75c
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-05-22 17:25:43 +0900

    0522

---
+++ hello.php
@@ -0,0 +1,30 @@
+<?php
+echo "hello world!" ;
+$a = 3 ;
+$b = 7 ;
+echo $a + $b;
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+$hello = "Hello,";
+$name = "Kazuya";
+$world = "'s World!";
+echo $hello . $name . $world;
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo $tech_boost;
+$calendar_2018 = [
+  "January" => "1月",
+  "Feburary" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "july" => "7月",
+  "August" => "8月",
+  "Septembar" => "9月",
+  "Octouber" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $calendar_2018["December"];
+?>
