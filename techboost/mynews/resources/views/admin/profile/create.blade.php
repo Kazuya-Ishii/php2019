98eaefcb724c6b5052641f4f510af5732b2186c0
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-05-23 17:14:56 +0900

    0523

---
+++ techboost/mynews/resources/views/admin/profile/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.profile')
+@section('title','Profileの新規作成')
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>My Profile</h2>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ techboost/mynews/resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=devise-width, initial-scale=1">
+
+  <title>Edit Profile</title>
+</head>
+  <body>
+    <h1>Profile編集画面</h1>
+  </body>
+</html>
